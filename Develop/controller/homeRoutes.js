cd8fa81675231ec9410bd29f4dfc0928643d7604
Carl Benbow <cbenbow@synergisticagility.com>
2021-06-30 07:41:00 -0400

    the home routes are working

--- Develop/controller/homeRoutes.js
+++ Develop/controller/homeRoutes.js
@@ -1,15 +1,95 @@
 const router = require("express").Router();
+var path = require("path");
+
+
 //exercise home routes
+router.get("/exercise", function(req, res) {
+    try{
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+ } catch (err) {
+          res.status(500).json(err);
+        }
+      });
+
+
+
+      router.get("/exercise/:id", function(req, res) {
+        try{
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
+     } catch (err) {
+              res.status(500).json(err);
+            }
+          });
+
+          router.get("/stats", function(req, res) {
+            try{
+                res.sendFile(path.join(__dirname, "../public/stats.html"));
+         } catch (err) {
+                  res.status(500).json(err);
+                }
+              });
+
+
+//   router.get("/exercise/:id", function(req, res) {
+
+//     res.sendFile(path.join(__dirname, "../public/exercise.html"));
+//   });
+//   router.get("/stats", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/stats.html"));
+//   });
 
 
 
 
 
+// router.get("/exercise", async (req, res) => {
+//     try {
+//       const blogData = await BlogPost.findAll({});
+//       const blogs = blogData.map((blogPost) => {
+//         return blogPost.get({ plain: true });
+//       });
+//       res.render("exercise.html", {
+       
+//       });
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
+  
 
-//index home route
+  
+// //   router.get("/exercise/:id", async (req, res) => {
+// //       try {
+// //         // const blogData = await BlogPost.findAll({});
+// //         // const blogs = blogData.map((blogPost) => {
+// //         //   return blogPost.get({ plain: true });
+// //         // });
+// //         res.res.sendFile("exercise.html", {
+         
+// //         });
+// //       } catch (err) {
+// //         res.status(500).json(err);
+// //       }
+// //     });
+
+// // //index home route
+
+
+// // //stats homeRoute
+// // router.get("/stats", async (req, res) => {
+// //     try {
+// //     //   const blogData = await BlogPost.findAll({});
+// //     //   const blogs = blogData.map((blogPost) => {
+// //     //     return blogPost.get({ plain: true });
+// //     //   });
+// //       res.render("../stats.html", {
+        
+// //       });
+// //     } catch (err) {
+// //       res.status(500).json(err);
+// //     }
+// //   });
 
 
 module.exports = router;
 
-
-//stats homeRoute
--- Develop/server.js
+++ Develop/server.js
@@ -2,21 +2,23 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const routes = require("./controller");
+const path = require('path')
 
 
 const PORT = process.env.PORT || 3001;
-
-
-
 const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "html");
+
 
 app.use(logger("dev"));
 app.use(routes);
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", { 
     useNewUrlParser: true,
